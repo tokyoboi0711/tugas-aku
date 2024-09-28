613d2761a50f543f258a8a0be5a496589f027098
tokyoboi0711 <azmididul@gmail.com>
2024-09-28 12:36:58 +0700

    anjay

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./**/*{.html,.js}"],
-	theme: {
-		extend: {
-			fontFamily: {
-				inter: ["Inter", "sans-serif"],
-				roboto: ["Roboto", "sans-serif"],
-        sserif: ["Source Serif 4", "serif"],
-			},
-		},
-	},
-	plugins: [],
+  content: ["./**/*{.html,.js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        crimson: ["Crimson Text", "serif"],
+      },
+      boxShadow: {
+        inner: "inset 0 2px 4px rgba(0, 0, 0, 0.1)",
+      },
+    },
+  },
+  plugins: [],
 };
 
